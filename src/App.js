88a95d97c35add7864818ceae73276aea70f43bd
Atharva Lakhamade <102431866+atha2810@users.noreply.github.com>
2024-07-03 21:17:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "../src/components/MyNavbar.jsx";
+import Features from "../src/components/Features.jsx";
+import HomeScreen from "../src/components/HomeScreen.jsx";
+import AddPage from "../src/components/AddPage.jsx";
+import Testimonials from "../src/components/Testimonials.jsx";
+import End from "../src/components/End.jsx";
+import Footer from "../src/components/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNavbar />
+      <HomeScreen />
+      <Features />
+      <AddPage />
+      <Testimonials />
+      <End />
+      <Footer />
     </div>
   );
 }
